0ac46e260244a1312a48fb6eb91fb685ca77a0cb
shidenggui <longlyshidenggui@gmail.com>
2016-02-19 23:02:04 +0800

    feat: add python version check

--- easyquotation/api.py
+++ easyquotation/api.py
@@ -1,6 +1,11 @@
+import sys
 from .sina import Sina
 from .leverfun import Leverfun
 from .jsl import Jsl
+
+PY_VERSION = sys.version_info[:2]
+if PY_VERSION < (3, 5):
+    raise Exception('Python 版本需要 3.5 或以上, 当前版本为 %s.%s 请升级 Python' % PY_VERSION)
 
 
 def use(source):
