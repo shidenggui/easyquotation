270e12b65e5891e6bd78275537cad36d092a9fe1
shidenggui <longlyshidenggui@gmail.com>
2016-10-13 11:03:44 +0800

    fix: reuse aiohttp client session to prevent crash
    fix #18

--- easyquotation/basequotation.py
+++ easyquotation/basequotation.py
@@ -13,6 +13,7 @@ class BaseQuotation:
     stock_api = ''  # 股票 api
 
     def __init__(self):
+        self._session = None
         stock_codes = self.load_stock_codes()
         self.stock_list = self.gen_stock_list(stock_codes)
 
@@ -28,7 +29,8 @@ class BaseQuotation:
             stock_list.append(request_list)
         return stock_list
 
-    def load_stock_codes(self):
+    @staticmethod
+    def load_stock_codes():
         with open(helpers.stock_code_path()) as f:
             return json.load(f)['stock']
 
@@ -47,12 +49,14 @@ class BaseQuotation:
         headers = {
             'Accept-Encoding': 'gzip'
         }
-        async with aiohttp.ClientSession().get(self.stock_api + params, timeout=5, headers=headers) as r:
+        async with self._session.get(self.stock_api + params, timeout=5, headers=headers) as r:
             response_text = await r.text()
             return response_text
 
     def get_stock_data(self, stock_list):
+        self._session = aiohttp.ClientSession()
         coroutines = []
+
         for params in stock_list:
             coroutine = self.get_stocks_by_range(params)
             coroutines.append(coroutine)
@@ -63,6 +67,7 @@ class BaseQuotation:
             asyncio.set_event_loop(loop)
         res = loop.run_until_complete(asyncio.gather(*coroutines))
 
+        self._session.close()
         return self.format_response_data(res)
 
     def format_response_data(self, rep_data):
