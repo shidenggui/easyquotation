12ac33c361649534ac09ca9767d89745e33b0c38
shidenggui <903618848@qq.com>
2018-01-08 20:49:10 +0800

    refactor: use threadpool replace asyncio

--- easyquotation/basequotation.py
+++ easyquotation/basequotation.py
@@ -1,11 +1,10 @@
 # coding:utf8
-import asyncio
 import json
 import warnings
+from multiprocessing.pool import ThreadPool
 
-import aiohttp
 import easyutils
-import yarl
+import requests
 
 from . import helpers
 
@@ -16,7 +15,7 @@ class BaseQuotation:
     stock_api = ''  # 股票 api
 
     def __init__(self):
-        self._session = None
+        self._session = requests.session()
         stock_codes = self.load_stock_codes()
         self.stock_list = self.gen_stock_list(stock_codes)
 
@@ -51,19 +50,8 @@ class BaseQuotation:
         """return quotation with stock_code prefix key"""
         return self.get_stock_data(self.stock_list, prefix=True)
 
-    def stocks(self, stock_codes):
-        if type(stock_codes) is not list:
-            stock_codes = [stock_codes]
-
-        stock_list = self.gen_stock_list(stock_codes)
-        return self.get_stock_data(stock_list)
-
-    def fetch_stocks(self, stock_codes):
-        if type(stock_codes) is not list:
-            stock_codes = [stock_codes]
-
-        stock_list = self.gen_stock_list(stock_codes)
-        return self.get_stock_data(stock_list, prefix=True)
+    def stocks(self, stock_codes, prefix=False):
+        return self.real(stock_codes, prefix)
 
     def real(self, stock_codes, prefix=False):
         """return specific stocks real quotation
@@ -84,34 +72,18 @@ class BaseQuotation:
         """
         return self.get_stock_data(self.stock_list, prefix=prefix)
 
-    async def get_stocks_by_range(self, params):
-        if self._session is None:
-            self._session = aiohttp.ClientSession()
+    def get_stocks_by_range(self, params):
         headers = {
             'Accept-Encoding': 'gzip, deflate, sdch',
             'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.100 Safari/537.36'
         }
-        url = yarl.URL(self.stock_api + params, encoded=True)
-        try:
-            async with self._session.get(url, timeout=10, headers=headers) as r:
-                response_text = await r.text()
-                return response_text
-        except asyncio.TimeoutError:
-            return None
+
+        r = self._session.get(self.stock_api + params, headers=headers)
+        return r.text
 
     def get_stock_data(self, stock_list, **kwargs):
-        coroutines = []
-
-        for params in stock_list:
-            coroutine = self.get_stocks_by_range(params)
-            coroutines.append(coroutine)
-        try:
-            loop = asyncio.get_event_loop()
-        except RuntimeError:
-            loop = asyncio.new_event_loop()
-            asyncio.set_event_loop(loop)
-        res = loop.run_until_complete(asyncio.gather(*coroutines))
-
+        pool = ThreadPool(len(stock_list))
+        res = pool.map(self.get_stocks_by_range, stock_list)
         return self.format_response_data([x for x in res if x is not None], **kwargs)
 
     def __del__(self):
