fbb1f035d6658422e4b44f38df2fe51b05ce7a1b
shidenggui <903618848@qq.com>
2018-04-11 21:44:51 +0800

    :star: add python 3.4 support

--- easyquotation/api.py
+++ easyquotation/api.py
@@ -1,14 +1,9 @@
 # coding:utf8
-import sys
 
 from .boc import Boc
 from .jsl import Jsl
 from .sina import Sina
 from .tencent import Tencent
-
-PY_VERSION = sys.version_info[:2]
-if PY_VERSION < (3, 5):
-    raise Exception('Python 版本需要 3.5 或以上, 当前版本为 %s.%s 请升级 Python' % PY_VERSION)
 
 
 def use(source):
