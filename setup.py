74c971b302362f6559c9d087b7cdea975795da22
shidenggui <903618848@qq.com>
2019-03-12 23:45:28 +0800

    Bump version: 0.5.12 → 0.5.13

--- setup.py
+++ setup.py
@@ -175,7 +175,7 @@ easyquotation.update_stock_codes()
 
 setup(
     name="easyquotation",
-    version="0.5.12",
+    version="0.5.13",
     description="A utility for Fetch China Stock Info",
     long_description=long_desc,
     author="shidenggui",
