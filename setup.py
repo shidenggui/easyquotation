0c8aefc5d577e9834f43b06878cae4de9b66fe0e
shidenggui <longlyshidenggui@gmail.com>
2025-03-21 21:23:44 +0800

    Bump version: 0.7.5 → 0.7.6

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="easyquotation",
-    version="0.7.5",
+    version="0.7.6",
     description="A utility for Fetch China Stock Info",
     long_description=long_description,
     long_description_content_type="text/markdown",
